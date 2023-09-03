244eea4a693545ce6ea29e635265a703ebc8a93f
Mohsin Maqbool <124125090+Mohsin-Maqbool@users.noreply.github.com>
2023-09-03 11:58:20 +0500

    Calculator Update

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-function press(e){
-    document.getElementById('input').value+=e;
-  }
-  function eq(){
-    var a = document.getElementById('input')
-    a.value = eval(a.value)
-  }
-  function clr() {
-      document.getElementById('input').value = ''
-   }
-  function ccc(){
-    var display = document.getElementById('input')
-    display.value = display.value.slice(0,-1)
-  }
-  
-  
+function press(e) {
+  document.getElementById('input').value += e;
+}
+
+function eq() {
+  var a = document.getElementById('input');
+  a.value = eval(a.value);
+}
+
+function clr() {
+  document.getElementById('input').value = '';
+}
+
+function ccc() {
+  var display = document.getElementById('input');
+  display.value = display.value.slice(0, -1);
+}
